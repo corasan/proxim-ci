5a7fd354769ecb12cfb806df4a7168d4a6e965e5
Henry Paulino <henrypl360@gmail.com>
2021-12-29 12:44:47 +0000

    build app and notify discord

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 extern crate prettylog;
 
 use std::net::TcpListener;
@@ -7,51 +9,59 @@ use serde::{Serialize, Deserialize};
 use std::process::Command;
 use structopt::StructOpt;
 use prettylog::*;
+use std::io::{self, Write};
+use std::str;
+use regex::Regex;
+use std::collections::HashMap;
 
 const PORT: &str = "7878";
 
 #[tokio::main]
 async fn main() {
+    let re = Regex::new(r"\D").unwrap();
     let args = Cli::from_args();
     let addr = format!("{}:{}", &args.bind, PORT);
     let listener = TcpListener::bind(addr).unwrap();
     let msg = format!("Server listening at: {}:{}", &args.bind, PORT);
     info(&msg);
 
-    // for stream in listener.incoming() {
-    //     let mut stream = stream.unwrap();
-    //     let mut buffer = [0; 1024];
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        let mut buffer = [0; 1024];
 
-    //     stream.read(&mut buffer).unwrap();
-    //     let git = Command::new("git").args(["pull", "--rebase", "origin", "main"]).output();
-    //     println!("{:?}", String::from_utf8(git.unwrap().stderr));
-    //     let build = Command::new("yarn").args(["build"]).output();
-    //     println!("{:?}", String::from_utf8(build.unwrap().stderr));
+        stream.read(&mut buffer).unwrap();
+        let git = Command::new("git")
+            .args(["pull", "--rebase", "origin", "main"])
+            .output()
+            .expect("failed to execute process");
+        
+        let error_m = format!("\x1B[31m{}\x1B[0m", String::from_utf8_lossy(&git.stderr));
+        let info_m = format!("\x1B[32m{}\x1B[0m", String::from_utf8_lossy(&git.stdout));
+        io::stdout().write_all(info_m.as_bytes()).unwrap();
+        io::stderr().write_all(error_m.as_bytes()).unwrap();
 
-        // println!("Request: {}",  )// (&buffer[..]));
-        // let serialized = String::from_utf8_lossy(&buffer[..]);
-        // println!("{:?}", serialized)
-        // let des: Hook = serde_json::(&serialized).unwrap();
-        // println!("{:?}", des);
-        // let mut map = HashMap::new();
-        // map.insert("content", "Successfully built and deployed app");
-        // let client = reqwest::Client::new();
-        // let url = "https://discord.com/api/webhooks/924634783785558028/MOvndSrWsSN0hJZMnYpcTy65KjHAxoBQfoTBQwwxesdb264JkBh48aKhhahAzmFtq1J7";
-        // let res = client.post(url).json(&map).send().await;
+        info("Building app");
+        let build = Command::new("yarn").current_dir(&args.path).args(["build"]).output().expect("Error!");
+        io::stdout().write_all(&build.stdout).unwrap();
+        io::stdout().write_all(&build.stderr).unwrap();
+        info("Build Finished");
 
-        // match res.unwrap().status() {
-        //     StatusCode::OK => println!("success!"),
-        //     StatusCode::PAYLOAD_TOO_LARGE => {
-        //         println!("Request payload is too large!");
-        //     }
-        //     s => println!("Received response status: {:?}", s),
-        // };
-    // }
+        let mut map = HashMap::new();
+        let name = "QuickRN";
+        let msg = format!("Successfully built and deployed {}", name);
+        map.insert("content", msg);
+        let client = reqwest::Client::new();
+        let url = "https://discord.com/api/webhooks/925729576091516938/fALUTGVZI7MbzyGUAojuOhN4t5pfP9gdxhftd-fDcIY05_FF5QjMnfRdgi-pLvwHeuVI";
+        let res = client.post(url).json(&map).send().await;
+    }
 }
 
 #[derive(StructOpt)]
 #[structopt(name = "proximo")]
 struct Cli {
     #[structopt(short = "b", long = "bind", default_value = "127.0.0.1")]
-    bind: String
+    bind: String,
+
+    #[structopt(short = "p", long = "path")]
+    path: String //std::path::PathBuf
 }
